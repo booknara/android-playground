623ccbe85f26ff071a751cf36dd5fe1464ec828e
Daehee Han <bookdori81@gmail.com>
2023-06-09 18:11:21 -0700

    Add more compose practice

--- compose/app/src/main/java/com/booknara/android/app/compose/MainActivity.kt
+++ compose/app/src/main/java/com/booknara/android/app/compose/MainActivity.kt
@@ -4,10 +4,17 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
@@ -16,10 +23,14 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.booknara.android.app.compose.data.Message
+import com.booknara.android.app.compose.data.SampleData
 import com.booknara.android.app.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -120,6 +131,90 @@ fun CardContent(name: String) {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun PreviewConversation() {
+    ComposeTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+//    LazyColumn {
+//        items(messages) {message ->
+//            MessageCard(msg = message)
+//        }
+//    }
+    Column {
+        messages.forEach { message -> 
+            val t = MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview(
+    name = "Dark Mode",
+    showBackground = true, 
+    widthDp = 320,
+    heightDp = 100, 
+    uiMode = UI_MODE_NIGHT_YES
+)
+@Composable
+fun PreviewMessageCard() {
+    ComposeTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MessageCard(msg = Message("Lexi", "Take a look at Jetpack Compose, it's great!"))
+        }
+    }
+}
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(painter = painterResource(id = R.drawable.profile_picture),
+        contentDescription = "photo",
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape))
+        
+        Spacer(modifier = Modifier.width(8.dp))
+        
+        var isExpanded by rememberSaveable {
+            mutableStateOf(false)
+        }
+        val surfaceColor by animateColorAsState(
+            targetValue = if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow)
+        )
+        Column {
+            Text(
+                text = msg.author, 
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            
+            Surface(
+                modifier = Modifier
+                    .clickable { isExpanded = !isExpanded }
+                    .animateContentSize()
+                    .padding(1.dp), 
+                shape = MaterialTheme.shapes.medium, 
+                shadowElevation = 1.dp,
+                color = surfaceColor
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }        
+    }
+}
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun MyAppPreview() {
---
+++ compose/app/src/main/java/com/booknara/android/app/compose/data/Message.kt
@@ -0,0 +1,3 @@
+package com.booknara.android.app.compose.data
+
+data class Message(val author: String, val body: String)
